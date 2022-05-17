5204615579bb8cc674bfec7f39853bf12a7e1842
hanifanggawi <hanifanggawi45@gmail.com>
2022-05-17 20:30:00 +0700

    feat: update Log schema

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -1,23 +1,40 @@
 import mongoose, { Document } from "mongoose";
 
+enum LogType {
+  INFO = 'INFO',
+  DEBUG = 'DEBUG',
+  ERROR = 'ERROR'
+}
+
 export interface ILog extends Document {
   created_at?: Date,
-  type: string,
+  type: LogType,
   service: string,
   message: string
 }
 
-export function isLog(arg: any) {
-  return typeof(arg?.type) === 'string' 
+export function isLog(arg: any): arg is ILog {
+  return arg?.type in LogType
   && typeof(arg?.service) === 'string' 
   && typeof(arg?.message) === 'string'
 }
 
 const LogSchema = new mongoose.Schema({
-  created_at: Date,
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
   type: String,
   service: String,
   message: String
+}, {
+  toJSON: {
+    transform: function (_doc, ret) {
+      ret.id = ret._id
+      delete ret._id
+      delete ret.__v
+    }
+  }
 });
 
 export class DB {
@@ -27,7 +44,7 @@ export class DB {
     console.log(`[...] Creating MongoDB Connection: ${process.env.DATABASE_URL}`)
     await mongoose.connect(process.env.DATABASE_URL!)
     console.log('[ ✓ ] MongoDB connected!')
-    this.logModel =  mongoose.model('LogItem', LogSchema)
+    this.logModel =  mongoose.model('Log', LogSchema)
   }
 
   static get LogModel() {
