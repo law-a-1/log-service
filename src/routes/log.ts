7f45b1ba3a71caf081f50b224e0888e9ca69b046
hanifanggawi <hanifanggawi45@gmail.com>
2022-05-27 12:48:20 +0700

    feat: add auth for get logs route

--- src/routes/log.ts
+++ src/routes/log.ts
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { getLog, getLogById, postLog } from "../controller/log"
+import { useAuth } from "../util/middleware"
 
 export function logRouter() {
   const route: Router = Router()
 
-  route.get('/', getLog)
-  route.get('/:id', getLogById)
+  route.get('/', useAuth, getLog)
+  route.get('/:id', useAuth, getLogById)
   route.post('/', postLog)
 
   return route
